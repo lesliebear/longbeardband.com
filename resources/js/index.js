df844e13c8faf3eb056a5d32d2a344aed3f86259
Tian Qi Xiong <tianqi.xiong@gmail.com>
2019-07-28 20:16:56 -0400

    First commit

---
+++ resources/js/index.js
@@ -0,0 +1,60 @@
+$(document).ready(() => {
+    if ($(window).height() > $('body').height()){
+      console.log('test');
+        $('body').height( $(window).height() );
+        // $('#body-content').height( $(window).height() );
+    }
+
+    $('.nav-item').mouseenter( () => {
+        $('.apple-whole').hide();
+        $('.apple-bite').show();
+    }).mouseleave( () => {
+        $('.apple-bite').hide();
+        $('.apple-whole').show();
+    });
+
+    twitch();
+});
+
+const twitch = () => {
+  setTimeout( () => {
+    $('.apple-whole').hide();
+    $('.apple-bite').show();
+  }, 200);
+  setTimeout( () => {
+    $('.apple-bite').hide();
+    $('.apple-whole').show();
+  }, 500);
+  setTimeout( () => {
+    $('.apple-whole').hide();
+    $('.apple-bite').show();
+  }, 700);
+  setTimeout( () => {
+    $('.apple-bite').hide();
+    $('.apple-whole').show();
+  }, 1000);
+  setTimeout( () => {
+    $('.apple-whole').hide();
+    $('.apple-bite').show();
+  }, 1200);
+  setTimeout( () => {
+    $('.apple-bite').hide();
+    $('.apple-whole').show();
+  }, 1500);
+  setTimeout( () => {
+    $('.apple-whole').hide();
+    $('.apple-bite').show();
+  }, 1700);
+  setTimeout( () => {
+    $('.apple-bite').hide();
+    $('.apple-whole').show();
+  }, 2000);
+  setTimeout( () => {
+    $('.apple-whole').hide();
+    $('.apple-bite').show();
+  }, 2200);
+  setTimeout( () => {
+    $('.apple-bite').hide();
+    $('.apple-whole').show();
+  }, 2500);
+}
